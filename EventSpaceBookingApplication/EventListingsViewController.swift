c98fe38fb1433512d76500a78ebac941a4346acf
Ramesh619 <apvs.ramesh@gmail.com>
2023-04-22 19:56:53 -0500

    EventListView Started
    Just created this view

---
+++ EventSpaceBookingApplication/EventListingsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EventListingsViewController.swift
+//  EventSpaceBookingApplication
+//
+//  Created by Adapa,Pydi Venkata Satya Ramesh on 4/22/23.
+//
+
+import UIKit
+
+class EventListingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
