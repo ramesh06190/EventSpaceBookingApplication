6f19e67b2090aa62ab3d14e9a89d0e91bb23ae8d
Ramesh619 <apvs.ramesh@gmail.com>
2023-04-24 23:37:39 -0500

    Added view controllers for some views

---
+++ EventSpaceBookingApp/EventSpaceBookingApp/BookingDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BookingDetailsViewController.swift
+//  EventSpaceBookingApp
+//
+//  Created by Adapa,Pydi Venkata Satya Ramesh on 4/24/23.
+//
+
+import UIKit
+
+class BookingDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
